0f019288db3b6aa0818875936fce2f25f7f324dd
Félix <fwilhelmy@hotmail.com>
2023-07-17 17:11:18 -0400

    Notification system creation

---
+++ src/main/java/ca/etsmtl/leakageanalysisplugin/notifications/Notifier.java
@@ -0,0 +1,31 @@
+package ca.etsmtl.leakageanalysisplugin.notifications;
+
+import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.NotificationType;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+
+public class Notifier
+{
+
+    public static void notifyInformation(String title, String content)
+    {
+        notify("TestNotification", title, content, NotificationType.INFORMATION);
+    }
+
+    public static void notifyError(String title, String content)
+    {
+        notify("TestNotification", title, content, NotificationType.ERROR);
+    }
+
+    private static void notify(String group, String title, String content, NotificationType type)
+    {
+        Project project = ProjectManager.getInstance().getOpenProjects()[0];
+
+        NotificationGroupManager.getInstance()
+                .getNotificationGroup(group)
+                .createNotification(title, content, type)
+                .notify(project);
+    }
+
+}
