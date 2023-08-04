fcc3fbccb4925f4b2023a20266ae9a2a5a9e2eed
guipreg <guillaume.pregent.2@ens.etsmtl.ca>
2023-08-03 20:45:09 -0400

    Added LeakageInstanceTest

--- src/test/java/ca/etsmtl/leakageanalysisplugin/models/leakage/LeakageInstanceTest.java
+++ src/test/java/ca/etsmtl/leakageanalysisplugin/models/leakage/LeakageInstanceTest.java
@@ -1,4 +1,20 @@
 package ca.etsmtl.leakageanalysisplugin.models.leakage;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LeakageInstanceTest {
+
+    private LeakageInstance leakageInstance;
+    private String filePath = "../testData/testNotebook.ipynb";
+
+    @Test
+    public void testLeakageInstance(){
+        leakageInstance = new LeakageInstance(filePath, 55);
+
+        assertEquals(leakageInstance.filePath, filePath);
+        assertEquals(leakageInstance.line, 55);
+    }
+
 }
